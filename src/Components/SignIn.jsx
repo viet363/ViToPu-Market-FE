8528132dc3130d10eea403ea55c445cc1602bb63
Lê Quốc Việt <lequocviet362003@gmail.com>
2024-09-06 11:53:34 +0700

    6/9

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 
 export default function SignIn() {
   return (
-    <div className=" bg-cover bg-center  flex items-center justify-center mt-[150px]">
+    <div class=" bg-cover bg-center  flex items-center justify-center mt-[150px]">
       <div>
-        <div className="bg-black bg-opacity-30 p-6 rounded-3xl w-96 items-center">
-          <h2 className="text-white font-bold text-center text-2xl mb-6">ĐĂNG NHẬP</h2>
-          <div className="border-t-8 border-[#FFFFFF] mb-8 rounded-full w-[255px] mx-auto"></div>
-          <form className="flex flex-col items-center">
-            <div className="mb-4 ">
+        <div class="bg-black bg-opacity-30 p-6 rounded-3xl w-[500px] items-center">
+          <h2 class="text-white font-bold text-center text-2xl mb-6">ĐĂNG NHẬP</h2>
+          <div class="border-t-8 border-[#FFFFFF] mb-8 rounded-full w-[255px] mx-auto"></div>
+          <form class="flex flex-col items-center">
+            <div class="mb-4 ">
               <input
-                className=" h-[50px] p-3 text-white  rounded-2xl"
+                class=" h-[50px] p-3 text-white  rounded-2xl"
                 type="email"
                 placeholder="Email"
               />
             </div>
-            <div className="mb-4 ">
+            <div class="mb-4 ">
               <input
-                className=" h-[50px] p-3 text-white  rounded-2xl"
+                class=" h-[50px] p-3 text-white  rounded-2xl"
                 type="password"
                 placeholder="Mật khẩu"
               />
             </div>
-            <div className="mb-6">
+            <div class="mb-6">
               <button
-                className="w-[150px] p-3 bg-[#458FFF] text-white rounded-3xl mt-11 font-bold"
+                class="w-[150px] p-3 bg-[#458FFF] text-white rounded-3xl mt-11 font-bold"
                 type="submit"
               >
                 Xác nhận
@@ -32,8 +32,8 @@ export default function SignIn() {
             </div>
           </form>
         </div>
-        <div className="text-center mt-4">
-          <a href="Signup" className="w-[350px] p-3 bg-[#7CD0FF] text-center rounded-3xl ">
+        <div class="text-center mt-4">
+          <a href="Signup" class="w-[350px] p-3 bg-[#7CD0FF] text-center rounded-3xl ">
             Đăng ký nếu chưa có tài khoản
           </a>
         </div>
