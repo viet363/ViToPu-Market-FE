4166704d9ad61fd767a8dd62205a20ac4e846275
Lê Quốc Việt <lequocviet362003@gmail.com>
2024-10-16 16:55:43 +0700

    16/10

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-export default function Home() {
+export default function SignIn() {
   const [user, setUser] = useState({
     email: "", matKhau: ""
   })
