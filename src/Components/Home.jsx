4dec36a45e56191068c73531a039ac43f4c92d71
Lê Quốc Việt <lequocviet362003@gmail.com>
2024-11-09 20:56:03 +0700

    9/11

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,115 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect, useRef } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const widthScreen = window.innerWidth;
+  const [scrollPage, setScrollPage] = useState(0);
+  const [listProduct, setListProduct] = useState([]);
+  const [valueSearch, setvalueSearch] = useState("");
+  const scrollProduct = useRef(null);
+
+  const debounce = (func, delay) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const debounceChange = useRef(
+    debounce((value) => {
+      setvalueSearch(value);
+    }, 1000)
+  );
+
+  const ChangeInputSearch = (e) => {
+    debounceChange.current(e.target.value);
+  };
+
+  const ChangeFirstScroll = () => {
+    if (scrollPage === 0) {
+      setScrollPage(2);
+    } else {
+      setScrollPage(scrollPage - 1);
+    }
+  };
+
+  const ChangeLastScroll = () => {
+    if (scrollPage === 2) {
+      setScrollPage(0);
+    } else {
+      setScrollPage(scrollPage + 1);
+    }
+  };
+
+  const addToCart = (idp) => {
+    const id = window.localStorage.getItem("ID");
+    if (id) {
+      axios
+        .post("http://localhost:9000/GioHang/AddToCart", {
+          maKhachHang: id,
+          maSanPham: idp,
+        })
+        .then((rs) => {
+          if (rs.data.Status === "Success") {
+            Navigate("/Cart");
+          } else {
+            alert("Something Wrong");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      Navigate("/SignIn");
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      ChangeLastScroll();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [scrollPage]);
+
+  const TakeList = () => {
+    axios
+      .post("http://localhost:9000/SanPham/Products")
+      .then((rs) => {
+        setListProduct(rs.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const TakeListSearch = () => {
+    axios
+      .post("http://localhost:9000/SanPham/Search", { Search: valueSearch })
+      .then((rs) => {
+        setListProduct(rs.data);
+      })
+      .catch((err) => {
+        setListProduct([]);
+      });
+  };
+
+  const Navigate = useNavigate();
+
+  useEffect(() => {
+    if (valueSearch) {
+      TakeListSearch();
+    } else {
+      TakeList();
+    }
+  }, [valueSearch]);
+
   return (
     <div className="flex flex-col overflow-hidden">
       <div className="flex items-center justify-center w-full h-[500px] bg-gradient-sky">
@@ -94,19 +203,79 @@ export default function Home() {
             onChange={ChangeInputSearch}
           ></input>
         </div>
-        <div class="bg-white bg-opacity-30 flex items-center justify-center">
-        <div class="grid-container grid grid-cols-1 gap-[30px] h-[600px] w-[300px] mt-4 overflow-auto scrollbar-hidden ">
-          {Array(3).fill(0).map((_, index) => (
-            <div key={index} class="bg-white rounded-lg p-4">
-              <button class="bg-[#D9D9D9] h-32 mb-2 rounded-md text-center w-full flex justify-center items-center">Ảnh sản phẩm</button>
-              <div class="flex justify-between items-center">
-                <span class="text-yellow-500 font-semibold">⭐ 5</span>
-                <div class="text-[#FF7816] text-right font-bold">100,000 lượt</div>
-              </div>
+        {listProduct.length === 0 ? (
+          <div className="flex flex-col gap-10 justify-center items-center w-full h-full">
+            <div>
+              <svg
+                className="w-[100px] font-extrabold opacity-70"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+              >
+                <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+              </svg>
             </div>
-          ))}
-        </div>
-        </div>
+            <div>
+              <p className="text-[40px] font-extrabold opacity-70">
+                Không có sản phẩm
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="grid grid-cols-4 w-full p-7 h-full overflow-auto bg-white gap-[30px]"
+            ref={scrollProduct}
+          >
+            {listProduct.map((p) => (
+              <div>
+                <div
+                  onClick={() => {
+                    window.localStorage.setItem("IDPP", p._id);
+                    Navigate("/PageProduct");
+                  }}
+                  className="flex flex-col duration-200 w-[450px] ease-linear hover:text-[rgba(83,165,185,1)] hover:shadow-2xl cursor-pointer"
+                >
+                  <div className="relative overflow-hidden w-[450px] h-[300px]">
+                    <div className="absolute flex w-full h-full justify-center items-center">
+                      <div>
+                        <LazyLoadImage
+                          className="h-[300px] w-[450px]"
+                          effect="blur"
+                          src={
+                            "http://localhost:9000/Image/" +
+                            p.hinhAnh +
+                            "." +
+                            p.loaiAnh
+                          }
+                          alt={p.tenSanPham}
+                        ></LazyLoadImage>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p-2">
+                    <p>{p.tenSanPham}</p>
+                  </div>
+                  <div className="flex justify-between items-center w-full px-2 pb-2">
+                    <div>
+                      <p>{Intl.NumberFormat().format(p.giaTien)} VND</p>
+                    </div>
+                    <div>
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          addToCart(p._id);
+                        }}
+                        disabled={window.localStorage.getItem("IDS") === p.maCuaHang ? true : false}
+                        className={window.localStorage.getItem("IDS") === p.maCuaHang ? "text-white p-2 bg-slate-300" :"bg-[rgba(83,165,185,1)] text-white p-2 border-[rgba(83,165,185,1)] border-[2px] duration-200 ease-linear hover:bg-white hover:text-[rgba(83,165,185,1)]"}
+                      >
+                        Thêm
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -106,34 +106,48 @@ export default function SignUp() {
   };
 
   return (
-    <div class="flex flex-col items-center justify-center min-h-screen bg-cover bg-center" >
-      <div class="bg-[#253133] bg-opacity-70 rounded-lg p-8 w-[1150px] h-[700px] flex flex-col justify-center items-center">
-        <div class="text-center mb-8 ">
-          <h2 class="text-2xl font-bold text-center  text-white">ĐĂNG KÝ</h2>
-          <div class="border-t-8 mt-3 border-[#e5e5e5] rounded-full w-[255px]"></div>
-        </div>
-        <div class="flex w-full justify-center items-center ml-20">
-          {/* Ảnh đại diện */}
-          <div class="w-1/3 flex flex-col items-center">
-            <div class="relative mb-8">
-              <button class="bg-gray-200 rounded-full flex items-center justify-center w-[300px] h-[300px]">
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-[220px] h-[220px] text-gray-500" viewBox="0 0 512 512" fill="currentColor">
-                  <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z" />
-                </svg>
-              </button>
-              {/* input file */}
-              <div class=" overflow-hidden absolute bg-white w-[75px] h-[75px] rounded-full flex items-center justify-center right-[-10px] bottom-[-40px]">
-                <input type="file" class="absolute  rounded-full opacity-0 cursor-pointer" />
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-[40px] h-[40px]" viewBox="0 0 512 512" fill="blue">
-                  <path d="M149.1 64.8L138.7 96 64 96C28.7 96 0 124.7 0 160L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-256c0-35.3-28.7-64-64-64l-74.7 0L362.9 64.8C356.4 45.2 338.1 32 317.4 32L194.6 32c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
-                </svg>
+    <form
+      onSubmit={handleCreateAccount}
+      method="post"
+      enctype="multipart/form-data"
+    >
+      <div class="flex flex-col items-center justify-center min-h-screen bg-cover bg-center">
+        <div class="bg-[#253133] bg-opacity-70 rounded-lg p-8 w-[1150px] h-[700px] flex flex-col justify-center items-center ">
+          <div class="text-center mb-8 ">
+            <h2 class="text-2xl font-bold text-center  text-white">ĐĂNG KÝ</h2>
+            <div class="border-t-8 mt-3 border-[#e5e5e5] rounded-full w-[255px]"></div>
+          </div>
+          <div class="flex w-full justify-center items-center ">
+            <div class="w-1/2 flex flex-col items-center">
+              <div class="relative mb-8">
+                <button class="bg-gray-200 rounded-full relative w-[300px] h-[300px] overflow-hidden">
+                  <img
+                    src={imagePreview ? imagePreview : "Image/User.png"}
+                    alt="Preview"
+                    className="w-[500px]"
+                  />
+                </button>
+                <div class=" overflow-hidden absolute bg-white w-[75px] h-[75px] rounded-full flex items-center justify-center right-[-10px] bottom-[-40px]">
+                  <input
+                    type="file"
+                    class="absolute rounded-full opacity-0 cursor-pointer"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    required
+                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-[40px] h-[40px]"
+                    viewBox="0 0 512 512"
+                    fill="blue"
+                  >
+                    <path d="M149.1 64.8L138.7 96 64 96C28.7 96 0 124.7 0 160L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-256c0-35.3-28.7-64-64-64l-74.7 0L362.9 64.8C356.4 45.2 338.1 32 317.4 32L194.6 32c-20.7 0-39 13.2-45.5 32.8zM256 192a96 96 0 1 1 0 192 96 96 0 1 1 0-192z" />
+                  </svg>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="w-[10px] h-[500px] bg-[#00D1FF] rounded-full ml-24 "></div>
-          {/* Form đăng ký */}
-          <div class="w-1/2">
-            <form class="space-y-4 flex-col w-[450px] ml-20 ">
+            <div class="  w-[10px] h-[500px] bg-[#00D1FF] rounded-full"></div>
+            <div class="w-1/2 ml-5 space-y-4">
               <div>
                 <input
                   type="text"
@@ -252,20 +266,18 @@ export default function SignUp() {
                   </span>
                 </label>
               </div>
-            </form>
+            </div>
+          </div>
+          <div class="text-center  w-[150px] mt-5  ">
+            <button
+              type="submit"
+              className="bg-[#458FFF] border-2 border-[#458FFF] text-[25px] text-white rounded-2xl px-3 py-1 duration-200 ease-linear hover:bg-white hover:text-[#458FFF]"
+            >
+              Xác nhận
+            </button>
           </div>
         </div>
-        <div class="text-center  w-[150px] mt-5  ">
-          <button type="submit" class="w-full bg-[#458FFF] text-white font-semibold py-2 rounded-3xl ">
-            Xác nhận
-          </button>
-        </div>
       </div>
-      <div class="text-center mt-14">
-        <a href="/SignIn" class="w-[350px] p-3 bg-[#7CD0FF] text-center rounded-3xl">
-          Đăng nhập nếu đã có tài khoản
-        </a>
-      </div>
-    </div>
+    </form>
   );
 }
